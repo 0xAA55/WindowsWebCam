30dfcfa71fde45a2d27ab5e1789ad9ff86b0a142
0xAA55 <weixiaobao213@qq.com>
2024-05-02 19:12:52 +0800

    实现：例程：使用 WebCam 拍摄 100 张 JPG 存储到工程目录，然后退出。

---
+++ webcam/test.cpp
@@ -0,0 +1,37 @@
+﻿#include "webcam.hpp"
+
+#include <iostream>
+
+using namespace WindowsWebCamTypeLib;
+
+struct UserStruct
+{
+	int NumFrames = 0;
+};
+
+void OnFrame(void* Userdata, WebCamType& wc, bool FrameUpdated)
+{
+	auto& US = *reinterpret_cast<UserStruct*>(Userdata);
+	if (FrameUpdated)
+	{
+		wc.GetFrameBuffer().SaveToJPG(std::string("test_") + std::to_string(++US.NumFrames) + ".jpg", 100);
+	}
+	if(US.NumFrames < 100) wc.QueryFrame();
+}
+
+int main()
+{
+	auto enumerated = WebCamType::EnumerateDevicesW();
+	for (auto& devpath : enumerated)
+	{
+		std::wcout << devpath << L"\n";
+	}
+
+	auto MyData = UserStruct();
+	auto WebCam = WebCamType(OnFrame, &MyData, true);
+	while (MyData.NumFrames < 100) std::cout << "";
+	
+
+	return 0;
+}
+

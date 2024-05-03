ad6a667a1e0907103aa58e611a370e4546881b22
0xAA55 <weixiaobao213@qq.com>
2024-05-03 10:08:51 +0800

    编写最小 Demo （还没到摄像头那块）

---
+++ glcamstream/test.cpp
@@ -0,0 +1,23 @@
+﻿
+#include "glfwwrap.hpp"
+
+int main(void)
+{
+    struct MyWebCamDemoWindowType : public GLFWWrap::GLFWwindowType
+    {
+        virtual bool OnMainLoop(uint32_t Width, uint32_t Height, double Time) override
+        {
+            float ratio;
+            auto& gl = GetMakeCurrent();
+            ratio = Width / (float)Height;
+            gl.Viewport(0, 0, Width, Height);
+            gl.Clear(gl.COLOR_BUFFER_BIT);
+
+            SwapBuffers();
+            return true;
+        }
+    };
+
+    auto MyDemo = MyWebCamDemoWindowType();
+    MyDemo.EnterMainLoop();
+}
